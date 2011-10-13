1249744d76187dca39ff44eba9934d967190e46c
Will Marshall <willrj.marshall@gmail.com>
2011-10-14 10:55:34 +1100

    First commit hooray

---
+++ gain_lower.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+require 'sinatra'
+
+
+get "/" do
+
+end
+
+get "/upload" do
+  # Parse file, return a download link
+end
+
+
+def parse_file(file)
+  "hello world"
+  #n = Nokogiri::XML(File.open("Psybreaks.nml"))
+
+  #n.xpath("//LOUDNESS").each do |loudness|
+  #  adjusted_db = (loudness.attributes["PEAK_DB"].value.to_f - 3.0).to_s
+  #  loudness.attributes["PEAK_DB"].value =  adjusted_db
+
+  #  adjusted_db = (loudness.attributes["PERCEIVED_DB"].value.to_f - 3.0).to_s
+  #  loudness.attributes["PERCEIVED_DB"].value =  adjusted_db
+
+  #  adjusted_db = (loudness.attributes["ANALYZED_DB"].value.to_f - 3.0).to_s
+  #  loudness.attributes["ANALYZED_DB"].value =  adjusted_db
+  #end
+
+  #File.open("adjusted_playlist.nml", "w") { |f| f.write(n.to_xml) }
+
+end
