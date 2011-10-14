3799ee2782aece1ddfd39f2021a641892c0e5f72
Will Marshall <willrj.marshall@gmail.com>
2011-10-14 12:02:56 +1100

    Removing debugger, adding some case handling

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'http://rubygems.org'
 gem 'sinatra'
 gem 'haml'
-gem 'ruby-debug19', :require => 'ruby-debug', :platform => :ruby_19
--- gain_lower.rb
+++ gain_lower.rb
@@ -7,17 +7,28 @@ get "/" do
 end
 
 post "/upload" do
-  #begin
-    playlist = params[:playlist][:tempfile]
-    updated_file = parse_file(playlist)
-    send_file(updated_file, :disposition => 'attachment', :filename => "test.nml")
-  #rescue
-   # "Your file was broken or something. Try exporting another one!"
-  #end
+  if params[:playlist]
+    begin
+      playlist = params[:playlist][:tempfile]
+      new_filename = generate_filename(params[:playlist][:filename])
+      gain_adjust = params[:gain].to_f
+
+      updated_file = parse_file(playlist, gain_adjust)
+
+      send_file(updated_file, :disposition => 'attachment', :filename => new_filename)
+    rescue
+      "Your file was broken or something. Try exporting another one!"
+    end
+  else
+    "You need to upload a playlist!"
+  end
 end
 
+def generate_filename(filename)
+  "Updated" + filename
+end
 
-def parse_file(file)
+def parse_file(file, gain_adjust)
   n = Nokogiri::XML(file.read)
 
   tempfile = Tempfile.new("temporary_file.txt", "/tmp")
@@ -25,13 +36,13 @@ def parse_file(file)
   n.xpath("//LOUDNESS").length.to_s
 
   n.xpath("//LOUDNESS").each do |loudness|
-    adjusted_db = (loudness.attributes["PEAK_DB"].value.to_f - 3.0).to_s
+    adjusted_db = (loudness.attributes["PEAK_DB"].value.to_f - gain_adjust).to_s
     loudness.attributes["PEAK_DB"].value =  adjusted_db
 
-    adjusted_db = (loudness.attributes["PERCEIVED_DB"].value.to_f - 3.0).to_s
+    adjusted_db = (loudness.attributes["PERCEIVED_DB"].value.to_f - gain_adjust).to_s
     loudness.attributes["PERCEIVED_DB"].value =  adjusted_db
 
-    adjusted_db = (loudness.attributes["ANALYZED_DB"].value.to_f - 3.0).to_s
+    adjusted_db = (loudness.attributes["ANALYZED_DB"].value.to_f - gain_adjust).to_s
     loudness.attributes["ANALYZED_DB"].value =  adjusted_db
   end
 
