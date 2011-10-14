18a4282e85731301d4faf777b31cd79e782cfc43
Will Marshall <willrj.marshall@gmail.com>
2011-10-14 11:44:57 +1100

    Adding a working first draft

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'http://rubygems.org'
 gem 'sinatra'
-
+gem 'haml'
+gem 'ruby-debug19', :require => 'ruby-debug', :platform => :ruby_19
--- gain_lower.rb
+++ gain_lower.rb
@@ -3,29 +3,38 @@ require 'sinatra'
 
 
 get "/" do
-  "Hello world"
+  haml :index
 end
 
-get "/upload" do
-  # Parse file, return a download link
+post "/upload" do
+  #begin
+    playlist = params[:playlist][:tempfile]
+    updated_file = parse_file(playlist)
+    send_file(updated_file, :disposition => 'attachment', :filename => "test.nml")
+  #rescue
+   # "Your file was broken or something. Try exporting another one!"
+  #end
 end
 
 
 def parse_file(file)
-  "hello world"
-  #n = Nokogiri::XML(File.open("Psybreaks.nml"))
+  n = Nokogiri::XML(file.read)
 
-  #n.xpath("//LOUDNESS").each do |loudness|
-  #  adjusted_db = (loudness.attributes["PEAK_DB"].value.to_f - 3.0).to_s
-  #  loudness.attributes["PEAK_DB"].value =  adjusted_db
+  tempfile = Tempfile.new("temporary_file.txt", "/tmp")
+  
+  n.xpath("//LOUDNESS").length.to_s
 
-  #  adjusted_db = (loudness.attributes["PERCEIVED_DB"].value.to_f - 3.0).to_s
-  #  loudness.attributes["PERCEIVED_DB"].value =  adjusted_db
+  n.xpath("//LOUDNESS").each do |loudness|
+    adjusted_db = (loudness.attributes["PEAK_DB"].value.to_f - 3.0).to_s
+    loudness.attributes["PEAK_DB"].value =  adjusted_db
 
-  #  adjusted_db = (loudness.attributes["ANALYZED_DB"].value.to_f - 3.0).to_s
-  #  loudness.attributes["ANALYZED_DB"].value =  adjusted_db
-  #end
+    adjusted_db = (loudness.attributes["PERCEIVED_DB"].value.to_f - 3.0).to_s
+    loudness.attributes["PERCEIVED_DB"].value =  adjusted_db
 
-  #File.open("adjusted_playlist.nml", "w") { |f| f.write(n.to_xml) }
+    adjusted_db = (loudness.attributes["ANALYZED_DB"].value.to_f - 3.0).to_s
+    loudness.attributes["ANALYZED_DB"].value =  adjusted_db
+  end
 
+  File.open(tempfile, "w") { |f| f.write(n.to_xml) }
+  tempfile
 end
